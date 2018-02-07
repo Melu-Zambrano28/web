d7f6bb5c58af807737bae385ff8063b728c57fce
Melissa Zambrano <belenzambrano@hotmail.es>
2018-02-07 18:11:41 +0100

    Generazione del seriale del biglietto

--- src/it/fides/sportivo/repository/maino.java
+++ src/it/fides/sportivo/repository/maino.java
@@ -1,11 +1,13 @@
 package it.fides.sportivo.repository;
 
+import it.fides.sportivo.Util.GeneraSerialBiglietto;
 import it.fides.sportivo.Util.UtilClientDao;
 import it.fides.sportivo.services.ServiceStatoBiglietto;
 import it.fides.sportivo.servicesimplementation.ServiceStatoBigliettoDao;
 
 import java.sql.*;
 import java.util.GregorianCalendar;
+import java.util.UUID;
 
 public class maino {
 
@@ -25,7 +27,10 @@ public class maino {
         GregorianCalendar cal1 =UtilClientDao.trasformaDataSqlaUtil(risult);
         System.out.println("Sql a gregoriian: "+cal1.getGregorianChange());
        //ServiceStatoBigliettoDao.updateDescrizioneStato(1,"sconto per studenti");
-       ServiceStatoBigliettoDao.deleteDescrizioneStato(1);
+       String seriale= GeneraSerialBiglietto.getSeriale();
+        String seriale2= GeneraSerialBiglietto.getSeriale();
+       System.out.print(seriale+"\nSeriale 2 :"+seriale2);
+       System.out.println("lunghezze del seriale 1: "+seriale.length()+"\nseriale 2 lungh: "+seriale2.length());
 
 
 
