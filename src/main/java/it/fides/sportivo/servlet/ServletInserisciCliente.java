017f04807801c79769d5eddc2bd69fa470fc6214
Niccolo Vernocchi <niccolo.vernocchifides.it>
2018-02-14 11:40:07 +0100

    tentativo 7

---
+++ src/main/java/it/fides/sportivo/servlet/ServletInserisciCliente.java
@@ -0,0 +1,24 @@
+package it.fides.sportivo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletInserisciCliente extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String nome = req.getParameter("name");
+        String cognome = req.getParameter("cognome");
+        String data = req.getParameter("data-nascita");
+
+    }
+}
